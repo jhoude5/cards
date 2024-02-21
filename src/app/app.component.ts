87c04ae86589314ac68cd9b0ce2823ba9c7c3319
Jennifer Aube <62511019+jaube05@users.noreply.github.com>
2024-02-21 10:03:10 -0500

    adding components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardComponent } from './card/card.component';
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CardComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    { 
+      title: 'Neat Tree', 
+      image: 'assets/tree.jpeg',
+      username: 'nature', 
+      content: 'I saw this neat tree today'
+    },
+    { 
+      title: 'Snowy Mountain', 
+      image: 'assets/mountain.jpeg',
+      username: 'mountainlover', 
+      content: 'Here is a picture of a snowy mountain'
+    },
+    { 
+      title: 'Moutain Biking', 
+      image: 'assets/biking.jpeg',
+      username: 'biking12222', 
+      content: 'I did some biking today'
+    }
+  ]
 }
---
+++ src/app/card/card.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.css'
+})
+export class CardComponent {
+  @Input() title = '';
+  @Input() imageUrl = '';
+  @Input() username = '';
+  @Input() content = '';
+
+  constructor() {}
+
+  ngOnInit() {}
+}
